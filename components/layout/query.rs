dac5ac4be484697deba264d0871390d428c31fe5
stevennovaryo <steven.novaryo@gmail.com>
2025-03-18 16:02:47 +0800

    Unbloat find function
    Signed-off-by: stevennovaryo <steven.novaryo@gmail.com>

--- components/layout/query.rs
+++ components/layout/query.rs
@@ -3,9 +3,12 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 //! Utilities for querying the layout, as needed by layout.
+use std::collections::HashMap;
+use std::hash::RandomState;
 use std::sync::Arc;
 
 use app_units::Au;
+use base::id::PipelineId;
 use euclid::default::{Point2D, Rect};
 use euclid::{SideOffsets2D, Size2D, Vector2D};
 use itertools::Itertools;
@@ -37,6 +40,8 @@ use style::values::generics::position::AspectRatio;
 use style::values::specified::GenericGridTemplateComponent;
 use style::values::specified::box_::DisplayInside;
 use style_traits::{ParsingMode, ToCss};
+use webrender_api::units::LayoutPixel;
+use webrender_api::ExternalScrollId;
 
 use crate::dom::NodeExt;
 use crate::flow::inline::construct::{TextTransformation, WhitespaceCollapse};
@@ -46,11 +51,15 @@ use crate::fragment_tree::{
 use crate::geom::{PhysicalRect, PhysicalVec};
 use crate::taffy::SpecificTaffyGridInfo;
 
+
 pub fn process_content_box_request(
     requested_node: OpaqueNode,
     fragment_tree: Option<Arc<FragmentTree>>,
+    pipeline_id: PipelineId,
+    scroll_offsets: &HashMap<ExternalScrollId, Vector2D<f32, LayoutPixel>, RandomState>,
 ) -> Option<Rect<Au>> {
-    let rects = fragment_tree?.get_content_boxes_for_node(requested_node);
+    let rects =
+        fragment_tree?.get_content_boxes_for_node(requested_node, pipeline_id, scroll_offsets);
     if rects.is_empty() {
         return None;
     }
@@ -65,9 +74,11 @@ pub fn process_content_box_request(
 pub fn process_content_boxes_request(
     requested_node: OpaqueNode,
     fragment_tree: Option<Arc<FragmentTree>>,
+    pipeline_id: PipelineId,
+    scroll_offsets: &HashMap<ExternalScrollId, Vector2D<f32, LayoutPixel>, RandomState>,
 ) -> Vec<Rect<Au>> {
     fragment_tree
-        .map(|tree| tree.get_content_boxes_for_node(requested_node))
+        .map(|tree| tree.get_content_boxes_for_node(requested_node, pipeline_id, scroll_offsets))
         .unwrap_or_default()
 }
 
